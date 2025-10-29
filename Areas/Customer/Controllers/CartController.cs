d31ceefe5d992f4bdc4cf73bf12c158c92d9e03e
sagarms97 <sagarsarawari@gmail.com>
2025-10-29 12:26:47 +0530

    shopping cart1

---
+++ Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+using Bulky.Models.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace TrackMe.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ShoppingCartVM ShoppingCartVM { get; set; }
+
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+
+            ShoppingCartVM shopping = new()
+            {
+                ShoppingCartList = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId,
+                includeProperties: "Product")
+
+            };
+            foreach(var cart in ShoppingCartVM.ShoppingCartList)
+            {
+                cart.price = GetPriceBasedOnQuantity(cart);
+                ShoppingCartVM.Ordertotal += (cart.price * cart.Count);
+            }
+
+            return View(ShoppingCartVM);
+        }
+
+        private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
+        {
+            if(shoppingCart.Count <=50)
+            {
+                return shoppingCart.Product.Price;
+            }
+            else
+            {
+                if(shoppingCart.Count <=100)
+                {
+                    return shoppingCart.Product.Price50;
+                }
+                else
+                {
+                    return shoppingCart.Product.Price100;
+                }
+            }
+        }
+            
+    }
+}
--- Areas/Customer/Controllers/HomeController.cs
+++ Areas/Customer/Controllers/HomeController.cs
@@ -1,7 +1,9 @@
 using Bulky.DataAccess.Repository.IRepository;
 using Bulky.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace TrackMe.Areas.Customer.Controllers
 {
@@ -22,10 +24,51 @@ namespace TrackMe.Areas.Customer.Controllers
             return View(productList);
         }
 
+        //public IActionResult Details(int productid)
+        //{
+
+        //    Product product = _unitOfWork.Product.Get(u => u.Id == productid, includeProperties: "Category");
+        //    return View(product);
+        //}
         public IActionResult Details(int productid)
         {
-            Product product = _unitOfWork.Product.Get(u=>u.Id== productid, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new ShoppingCart
+            {
+                Product = _unitOfWork.Product.Get(u => u.Id == productid, includeProperties: "Category"),
+                Count = 1,
+                ProductId = productid
+            };
+
+            return View(cart);
+        }
+
+        [HttpPost]
+        [Authorize]                     // we need user ID to store and they must be autorized user
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u=> u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+
+            if(cartFromDb != null)
+            {
+                //Shopping cart already exist
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.update(cartFromDb);
+                TempData["success"] = "Cart Updated SuccessFully";
+            }
+            else
+            {
+                //Cart to be added
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+                TempData["success"] = "Cart Added SuccessFully";
+            }
+
+           
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
         }
 
         public IActionResult Privacy()
